e5bc5b89825e5ffc1b165084b27bdd5624c70e73
vstoms <vstoms@gmail.com>
2019-05-13 21:58:37 +0200

    Changes in config.js

--- config.js
+++ config.js
@@ -2,12 +2,12 @@
 
 module.exports = {
   url: 'https://lumen.netlify.com',
-  title: 'Blog by John Doe',
+  title: 'keep IT tidy',
   subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: '1234UA-73379983-2',
   menu: [
     {
       label: 'Articles',
