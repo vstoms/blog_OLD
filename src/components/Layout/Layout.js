5feec5f163ac37eb0ade71ee8c6fbf1a2edf9511
vstoms <vstoms@gmail.com>
2019-05-22 20:24:41 +0200

    Update Layout.js

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -18,6 +18,7 @@ const Layout = ({ children, title, description }: Props) => (
       <meta name="description" content={description} />
       <meta property="og:site_name" content={title} />
       <meta name="twitter:card" content="summary" />
+      <meta name="twitter:title" content={title} />
     </Helmet>
     {children}
   </div>
